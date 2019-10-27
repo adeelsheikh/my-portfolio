9a0012831bb783c5818e2b54dff6f8966f5727ef
adsheikh <adeelnayyersheikh@gmail.com>
2019-10-27 22:45:35 +0400

    Update the portfolio upload lambda script. SNS notification is added for deployment status.

--- upload-portfolio.py
+++ upload-portfolio.py
@@ -7,19 +7,30 @@ import zipfile
 
 
 def lambda_handler(event, context):
-    s3 = boto3.resource('s3')
+    sns = boto3.resource('sns')
+    topic = sns.Topic(
+        'arn:aws:sns:eu-central-1:809411616619:Deploy-Portfolio-Topic')
+    try:
+        s3 = boto3.resource('s3')
+        portfolio_bucket = s3.Bucket('portfolio.adeelnayyer.uk')
+        build_bucket = s3.Bucket('portfoliobuild.adeelnayyer.uk')
 
-    portfolio_bucket = s3.Bucket('portfolio.adeelnayyer.uk')
-    build_bucket = s3.Bucket('portfoliobuild.adeelnayyer.uk')
+        portfolio_zip = BytesIO()
+        build_bucket.download_fileobj('portfolio-build.zip', portfolio_zip)
 
-    portfolio_zip = BytesIO()
-    build_bucket.download_fileobj('portfolio-build.zip', portfolio_zip)
+        with zipfile.ZipFile(portfolio_zip) as portfolioZip:
+            for name in portfolioZip.namelist():
+                obj = portfolioZip.open(name)
+                portfolio_bucket.upload_fileobj(
+                    obj, name, ExtraArgs={'ContentType': mimetypes.guess_type(name)[0]})
+                portfolio_bucket.Object(name).Acl().put(ACL='public-read')
 
-    with zipfile.ZipFile(portfolio_zip) as portfolioZip:
-        for name in portfolioZip.namelist():
-            obj = portfolioZip.open(name)
-            portfolio_bucket.upload_fileobj(
-                obj, name, ExtraArgs={'ContentType': mimetypes.guess_type(name)[0]})
-            portfolio_bucket.Object(name).Acl().put(ACL='public-read')
+        topic.publish(Subject="Deployment Successful",
+                      Message="Portfolio is deployed successfully.")
 
-    return 'Portfolio is deployed successfully.'
+    except:
+        topic.publish(Subject="Deployment Failed",
+                      Message="Failed to deploy portfolio.")
+        raise
+
+    return 'Done!'
